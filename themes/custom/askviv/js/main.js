167906e78aa6c234959c0f4b1b86c7da3a29fe1e
Martin Daskalov <mdaskalov@rippleffect.com>
2018-03-10 17:47:38 +0000

    Fix some issues with the header

--- themes/custom/askviv/js/main.js
+++ themes/custom/askviv/js/main.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-  alert('here');
+jQuery(document).ready(function($) {
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+
+      $(".site-branding__slogan").addClass("hidden");
+      $(".header__container").addClass("header__container-small");
+
+    } else {
+
+      $(".site-branding__slogan").removeClass("hidden");
+
+      $(".header__container").removeClass("header__container-small");
+    }
+  });
 });
